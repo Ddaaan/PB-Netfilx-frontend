4a489c7941da1eecdc5327c8f2148c55c26649e5
Ddaaan <de2572@naver.com>
2025-12-13 16:50:00 +0900

    chore: 주요 페이지 기본 뼈대 생성

---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,8 @@
+export default function SignInPage() {
+    return (
+        <div style={{ padding: 24 }}>
+            <h1>/signin</h1>
+            <p>다음 단계에서 로그인/회원가입 전환 애니메이션 + localStorage 인증 구현할 예정</p>
+        </div>
+    );
+}
