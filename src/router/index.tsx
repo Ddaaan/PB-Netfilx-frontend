59421c554f759b419b2399e0e94f991c377de7a0
Ddaaan <de2572@naver.com>
2025-12-13 16:50:06 +0900

    feat: SPA 라우터 구성 및 인증 필요 페이지 보호

---
+++ src/router/index.tsx
@@ -0,0 +1,35 @@
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "../components/layout/Layout";
+import { RequireAuth } from "../utils/auth.tsx";
+
+import SignInPage from "../pages/SignInPage";
+import HomePage from "../pages/HomePage";
+import PopularPage from "../pages/PopularPage";
+import SearchPage from "../pages/SearchPage";
+import WishlistPage from "../pages/WishlistPage";
+
+export function AppRouter() {
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/signin" element={<SignInPage />} />
+
+                <Route
+                    path="/"
+                    element={
+                        <RequireAuth>
+                            <Layout />
+                        </RequireAuth>
+                    }
+                >
+                    <Route index element={<HomePage />} />
+                    <Route path="popular" element={<PopularPage />} />
+                    <Route path="search" element={<SearchPage />} />
+                    <Route path="wishlist" element={<WishlistPage />} />
+                </Route>
+
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </HashRouter>
+    );
+}
